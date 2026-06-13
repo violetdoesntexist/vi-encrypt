2e0b5c52315943440c271a521e6ac29b48cb0c30
violetdoesntexist <violetdoesntexist11@gmail.com>
2026-06-13 18:25:49 +0200

    fixed text formatting

--- main.py
+++ main.py
@@ -12,33 +12,33 @@ Art = text2art("VIENCRYPT", chr_ignore=True)
 print(Fore.MAGENTA + Art)
 
 print(
-    "what do you want to do? :)"
-    "E = Encrypt"
-    "D = Decrypt"
-    "I = Project Info"
+    """what do you want to do? :)
+     E = Encrypt
+     D = Decrypt
+     I = Project Info"""
 )
 
-choice = input("choose:")
+choice = input("choose: ")
 if(choice == "D"):
-    encryption_key = input("enter key:")
-    string = input("enter string:")
+    encryption_key = input("enter key: ")
+    string = input("enter string: ")
     while(alphnum > -1 ):
         decrypted = string.replace(encryption_key[alphnum], alphabet[alphnum] )
-        print("replaced" + encryption_key[alphnum] + "with" + alphabet[alphnum] + decrypted)
+        print("replaced " + encryption_key[alphnum] + " with " + alphabet[alphnum] + " " + decrypted)
         alphnum = alphnum - 1
         time.sleep(0.5)
 
-    print("string" + string + "encrypted with key" + encryption_key)
-    print("result" + encrypted)
+    print("string " + string + " encrypted with key  " + encryption_key)
+    print("result " + encrypted)
 elif(choice == "E"):    
     encryption_key = ''.join(random.sample(alphabet, len(alphabet)))
     print("your key is:" + encryption_key)
-    string = input("enter string:")
+    string = input("enter string: ")
     while(alphnum > -1 ):
             encrypted = string.replace(alphabet[alphnum], encryption_key[alphnum] )
-            print("replaced" + encryption_key[alphnum] + "with" + alphabet[alphnum] + encrypted)
+            print("replaced " + encryption_key[alphnum] + " with " + alphabet[alphnum] + " " + encrypted)
             alphnum = alphnum - 1
             time.sleep(0.5)
-    print("your key is" + encryption_key)
-    print("your string is" + string)
+    print("your key is " + encryption_key)
+    print("your string is " + string)
     
